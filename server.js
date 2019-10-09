59be5892a2129c3e6b430331ea97876ecc017025
sebekos <sebkpl@gmail.com>
2019-10-08 19:30:30 -0500

    Staging

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+require('dotenv').config();
+
 const app = express();
 
 // Connect Database
@@ -10,14 +14,21 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => res.send('Api Running'));
-
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/text', require('./routes/api/text'));
 app.use('/api/photo', require('./routes/api/photo'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
