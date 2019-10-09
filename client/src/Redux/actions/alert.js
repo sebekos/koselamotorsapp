800f04e55c8cd346aeec8a538a4f594d4becb737
sebekos <sebkpl@gmail.com>
2019-10-08 22:16:22 -0500

    Updated alert

--- client/src/Redux/actions/alert.js
+++ client/src/Redux/actions/alert.js
@@ -5,23 +5,11 @@ export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
     const id = uuid.v4();
     dispatch({
         type: SET_ALERT,
-        payload: { msg, alertType, id }
+        payload: [{ msg, alertType, id }]
     });
 
     setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
-}
-
-export const setAllAlert = (msg, alertType, timeout = 5000) => dispatch => {
-    const id = uuid.v4();
-    // Remove all alerts first;
-    dispatch({ type: REMOVE_ALL_ALERTS })
-    dispatch({
-        type: SET_ALERT,
-        payload: { msg, alertType, id }
-    });
-
-    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
-}
+};
 
 export const removeAlerts = () => dispatch => {
     dispatch({ type: REMOVE_ALL_ALERTS });
--- client/src/Redux/reducers/alert.js
+++ client/src/Redux/reducers/alert.js
@@ -2,14 +2,11 @@ import { SET_ALERT, REMOVE_ALERT, REMOVE_ALL_ALERTS } from '../actions/types';
 
 const initialState = [];
 
-export default function (state = initialState, action) {
-    const { type, payload } = action;
+export default function (state = initialState, { type, payload }) {
     switch (type) {
         case SET_ALERT:
-            return [...state, payload];
+            return payload;
         case REMOVE_ALERT:
-            return state.filter(alert => alert.id !== payload);
-        case REMOVE_ALL_ALERTS:
             return [];
         default:
             return state;
